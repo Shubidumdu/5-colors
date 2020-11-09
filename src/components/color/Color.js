d38691c02d66899420c4abf8bb95bad6fd785dd2
Shubidumdu <skaty4870@naver.com>
2020-11-10 00:03:43 +0900

    Modify colorpicker logic

--- src/components/color/Color.js
+++ src/components/color/Color.js
@@ -5,8 +5,8 @@ const ColorCircle = styled.span`
   background: ${({ $background }) => $background};
 `;
 
-const Color = ({ color }) => {
-  return <ColorCircle data-testid="color" $background={color} />;
+const Color = ({ color, ...rest }) => {
+  return <ColorCircle data-testid="color" $background={color} {...rest} />;
 };
 
 export default Color;
--- src/components/colorpicker/ColorPicker.js
+++ src/components/colorpicker/ColorPicker.js
@@ -1,41 +1,70 @@
-import React, { useRef, useState } from "react";
-import styled from "styled-components";
-import { SliderPicker } from "react-color";
-import { usePopper } from "react-popper";
+import React, { useRef, useState } from 'react';
+import styled from 'styled-components';
+import { ChromePicker } from 'react-color';
+import { usePopper } from 'react-popper';
 
-const ColorPicker = ({ onClick }) => {
-  const [color, setColor] = useState("#fff");
+const Pop = styled.div`
+  display: ${(props) => (props.visible ? 'flex' : 'none')};
+  width: 12rem;
+  flex-direction: column;
+  border-radius: 4px;
+  padding: 5px;
+`;
+
+const PopContainer = styled.div`
+  width: 12rem;
+  background: #fff;
+  box-shadow: 0px 1px 6px lightgrey;
+`;
+
+const Color = styled.div`
+  width: 2rem;
+  height: 2rem;
+  border-radius: 2rem;
+  background: ${(props) => props.color};
+`;
+
+const ColorPicker = ({}) => {
+  const [color, setColor] = useState('#000');
   const onChange = (color, e) => {
-    setColor(color);
+    const hex = color.hex;
+    setColor(hex);
   };
 
-  const targetRef = useRef();
-  const popperRef = useRef();
-  const arrowRef = useRef();
+  const [visible, setVisibility] = useState(false);
+  const onClick = (e) => {
+    setVisibility((visible) => !visible);
+  };
+  const targetRef = useRef(null);
+  const popperRef = useRef(null);
 
   const { styles, attributes } = usePopper(
     targetRef.current,
     popperRef.current,
     {
+      placement: 'right',
       modifiers: [
         {
-          name: "arrow",
-          options: { element: arrowRef },
+          name: 'offset',
+          enabled: true,
+          options: {},
         },
       ],
-    }
+    },
   );
 
   return (
     <div>
-      <button type="button" ref={targetRef}>
-        테스트
-      </button>
-      <div ref={popperRef} style={styles.popper} {...attributes.popper}>
-        {/* <SliderPicker color={color} onChange={onChange} /> */}
-        테스트
-        <div ref={arrowRef} style={styles.arrow} />
-      </div>
+      <Color ref={targetRef} onClick={onClick} color={color} />
+      <PopContainer
+        ref={popperRef}
+        style={styles.popper}
+        {...attributes.popper}
+      >
+        <Pop style={styles.offset} visible={visible}>
+          <ChromePicker color={color} onChange={onChange} />
+        </Pop>
+      </PopContainer>
     </div>
   );
 };
